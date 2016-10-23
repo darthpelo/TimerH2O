d162d31a74f7d2fb4cdc4f0f42758338f6b44224
Alessio Roberto <darthpelo@gmail.com>
2016-10-23 23:25:19 +0200

    Creato Xcode project

---
+++ TimerH2O/TimerH2O/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TimerH2O
+//
+//  Created by Alessio Roberto on 23/10/16.
+//  Copyright © 2016 Alessio Roberto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
