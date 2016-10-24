48804f41e5c8ffe8ad5dcb84a04aaaa17d3d4afe
Alessio Roberto <darthpelo@gmail.com>
2016-10-24 22:26:17 +0200

    Add pods

---
+++ TimerH2O/Pods/Target Support Files/Pods-TimerH2O/Pods-TimerH2O-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TimerH2O : NSObject
+@end
+@implementation PodsDummy_Pods_TimerH2O
+@end
---
+++ TimerH2O/Pods/Target Support Files/Pods-TimerH2O/Pods-TimerH2O-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TimerH2OVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TimerH2OVersionString[];
+
